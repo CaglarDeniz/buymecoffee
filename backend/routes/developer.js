32f1bb4cf6c6257b557f26a5f8713a63e9da58ed
Deniz Caglar <denizcaglar200@gmail.com>
2022-11-22 15:33:29 -0600

    defined methods for dev and dev/:id

--- backend/routes/developer.js
+++ backend/routes/developer.js
@@ -4,7 +4,25 @@ const router = express.Router();
 var devRoute = router.route('/');
 
 devRoute.get(function (req, res) {
-res.json({ message: 'My connection string is dev'  });
+	res.json({ message: 'My connection string is dev'  });
 });
+
+devRoute.post(function (res,req){
+	res.json({message: 'Post request to dev'});	
+})
+
+var devIdRoute = router.route('/:id');
+
+devIdRoute.get(function(res,req){
+	res.json({message: 'Get request to dev id'});	
+})
+
+devIdRoute.post(function(res,req){
+	res.json({message: 'Post request to dev id'});
+})
+
+devIdRoute.delete(function(res,req){
+	res.json({message: 'Delete request to dev id'});
+})
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,4 +5,6 @@ module.exports = function (app) {
     app.use('/api/project', require('./project.js'));
     app.use('/api/developer', require('./developer.js'));
     app.use('/api/investor', require('./investor.js'));
+		app.use('/api/auth_developer',require('./auth_dev.js'));
+		app.use('/api/auth_investor',require('./auth_investor.js'));
 };
