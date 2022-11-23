0c7718e52902fc13d1907eaa6e55b47f35b93700
aisheem2 <aishee2001@gmail.com>
2022-11-22 19:47:09 -0600

    working on UserLogin view

--- src/InvestorGallery.js
+++ src/InvestorGallery.js
@@ -1,8 +1,10 @@
+import ResponsiveAppBar from './components/navbarGallery';
 
 function InvestorGallery(props) {
 
     return (
         <div className="container-wrap">
+        <ResponsiveAppBar/>
             <h1>hello investor gallery</h1>
         </div>
     );
