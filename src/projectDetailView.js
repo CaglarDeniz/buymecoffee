33b835c8a30b2c05d113dda52866e649ee0bb54b
aisheem2 <aishee2001@gmail.com>
2022-12-01 09:06:31 -0600

    project detailview working on

--- src/projectDetailView.js
+++ src/projectDetailView.js
@@ -1,13 +1,32 @@
 import { useParams } from "react-router";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function ProjectDetailView(props) {
     let params = useParams();
     console.log("params", params)
-    
+    const theme = createTheme({
+        palette: {
+          primary: {
+            main: "#313335",
+            grey: "#CACCCE",
+            blue: "#0077B5"
+          },
+          secondary: {
+            main: "#FFFFFF",
+          },
+        },
+        typography: {
+            fontFamily: [
+              'Roboto Mono',
+              'monospace',
+            ].join(','),
+          }
+    });
+
     return (
         <div className="container-wrap">
             <div className="userlogin-navbar"></div>
-            <h1>hello  project detail view {params.investorId}</h1>
+            <h1>hello  project detail view {params._id}</h1>
         </div>
     );
 }
