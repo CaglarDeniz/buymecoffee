3fa8a448e9d706404d59ecea135c5deb78de4278
chayanid Kaewla <ckaewla2@illinois.edu>
2022-12-02 16:28:03 -0600

    add public url as basename

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import SubmitProject from "./SubmitProject";
 import LogIn from "./logIn";
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/projects" element={<ProjectGallery />} />
         <Route path="/project/:projectId" element={<ProjectDetailView />} />
