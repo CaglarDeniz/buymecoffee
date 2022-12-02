12982918b2991654c303439f081f8ce4e19dd9b6
Sanjana <pingali4@illinois.edu>
2022-12-02 11:51:35 -0600

    add post endpoint to project

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -5,6 +5,9 @@ const Proj = require('../models/project.js');
 
 var projectRoute = router.route('/');
 
+const bcrypt = require('bcrypt'); // JS hashing library for password auth
+const saltRounds = 10;
+
 // GET / // Return all projects// ?where = Project Schema JSON
 // POST /// Put Project JSON in body to create// Returns created Project JSON
 // GET /:id // Get Project by ID // Returns single Project in data
@@ -19,7 +22,7 @@ projectRoute.get(async function (req, res) {
         query_where = JSON.parse(req.query['where'])
       }
     
-    await Proj.find(Investor.find(query_where, {passwordHash:0}), async function (err, result) {
+    await Proj.find(Proj.find(query_where, {passwordHash:0}), async function (err, result) {
     if (err){
         res.status(500).json({message:"Couldn't find Investors due to error" + err.message});
     }
@@ -29,7 +32,51 @@ projectRoute.get(async function (req, res) {
         });
     })
 
+projectRoute.post(async function (req, res) {
+        // find by username
+        try{
+        const curProject = await Proj.find({ username: req.body.username})
     
+        if (curProject.length != 0) { // if a dev with the same username exists
+            res.status(500).json({
+              message: "An investor with that username already exists",
+              data: []
+            });
+            return;
+          }
+          else{
+            bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+                Proj.create({ 
+                  name: req.body.name,
+                  industry: 'industry' in req.body ? req.body.industry : "",
+                  description: req.body.description,
+                  ownerId: 'ownerId' in req.body ? req.body.ownerId : "",
+                  amount: 'amount' in req.body ? req.body.amount : 0},
+                 async function (err, result) {
+                  if (err){
+                    res.status(500).json({
+                      message: "Couldn't save Project to database due to error " + err.message
+                    });
+                              }
+                else{
+                  result.passwordHash = undefined; // don't return passwordHash
+                    console.log(result);
+                res.status(201).json({
+                  message: "Added Project to database",
+                  data: result,
+                    })
+                  };
+                })
+                })
+              }
+          }
+          catch (err) {
+            res.status(500).json({
+              message: "Couldn't create Project due to error " + err.message
+            });
+            return;
+          }
+    });
     
 
 module.exports = router;
