31c9a9fe9f55376f1aa3b619b98699891b09b50d
Sanjana <pingali4@illinois.edu>
2022-12-01 20:55:10 -0600

    delete endpoint added for investors

--- backend/routes/investor.js
+++ backend/routes/investor.js
@@ -105,6 +105,23 @@ investorRouteId.get(async function (req, res) {
       });
   })
   
+  investorRouteId.delete(async function (req, res) {
+    if((await Investor.find({_id:req.params.id})).length ==0){
+        res.status(404).json({message:"Could not find User Id", data:{}});
+      }
+      else{
+
+    await Investor.deleteOne({ _id: req.params.id}, async function (err, result) {
+        if (err){
+            res.status(500).json({message:"Internal Server Error", "data":{}});
+        }
+        else{
+            res.status(200).json({message:"OK", "data":{}});
+                    }
+    }
+        );
+}
+});
 
 
 module.exports = router;
