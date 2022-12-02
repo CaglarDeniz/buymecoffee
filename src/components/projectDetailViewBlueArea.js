3288af8a17d6a10ac614dc24c8423c4d0e0d0c2d
aisheem2 <aishee2001@gmail.com>
2022-12-01 21:47:42 -0600

    detail view done

--- src/components/projectDetailViewBlueArea.js
+++ src/components/projectDetailViewBlueArea.js
@@ -4,11 +4,11 @@ function ProjectDetailViewBlueArea(props) {
     return (
         <div className="blue-area">
             <h5 className="box_d-text">Project Name</h5>
-            <h5>{props.project.name}</h5>
+            <h5 className='proj-details'>{props.project.name}</h5>
             <h5 className="box_d-text">Details</h5>
-            <h5>{props.project.description}</h5>
+            <h5 className='proj-details'>{props.project.description}</h5>
             <h5 className="box_d-text">Money</h5>
-            <h5>{props.project.amount}</h5>
+            <h5 className='proj-details'>{props.project.amount}</h5>
             <button className="pitch-coffee" onClick={() => window.location = 'mailto:np.js409@gmail.com'}>Buy Me Coffee</button>
         </div>
 
