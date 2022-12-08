962aa34b401a94d9967793fc464d373540764e52
chayanid Kaewla <ckaewla2@illinois.edu>
2022-12-08 00:36:54 -0600

    upload photo

--- src/Signup.js
+++ src/Signup.js
@@ -3,6 +3,7 @@ import "./signup.css";
 import { useState } from "react";
 import SelectAutoWidth from "./components/selectButton";
 import Axios from "axios";
+import Avatar from "@mui/material/Avatar";
 
 function Signup() {
   const navigate = useNavigate();
@@ -21,6 +22,7 @@ function Signup() {
     "Chemical",
     "Other",
   ];
+
   const roleOptions = ["Project Owner", "Investor"];
   const [username, setUsername] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -32,64 +34,85 @@ function Signup() {
   const [bio, setBio] = useState("");
   const [oldStartUp, setOldStartUp] = useState("");
   const [amount, setAmount] = useState(0);
-  // const [tempPhoto, setTempPhoto] = useState("");
-  const [photoLink, setPhotoLink] = useState("");
-  console.log(setPhotoLink) //TODO: comment this out
+  const [tempPhoto, setTempPhoto] = useState("");
+  // const [photoLink, setPhotoLink] = useState("");
+  console.log(tempPhoto);
   const handleSubmit = (event) => {
-    if(password === ""|| firstName==="" || LastName==="" || email===""  || email===""){
-      alert("FirstName, LastName, Username, email, and Password field are required.")
-      return
+    if (
+      password === "" ||
+      firstName === "" ||
+      LastName === "" ||
+      email === "" ||
+      email === ""
+    ) {
+      alert(
+        "FirstName, LastName, Username, email, and Password fields are required."
+      );
+      return;
     }
     let next = "/login";
     if (role === "Project Owner") {
-      Axios.post(`http://localhost:8080/api/developer/`, {
-        name: firstName + " " + LastName,
-        email: email,
-        password: password,
-        username: username,
-        industry: industry,
-        bio: bio,
-        photoLink: photoLink,
+      let formData = new FormData();
+      formData.append("photo", tempPhoto);
+      console.log("here is the temp", tempPhoto);
+      Axios.post(`http://localhost:8080/upload/`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
       })
         .then((res) => {
-          console.log(res);
-          navigate(next)
+          console.log("second post", res);
+          // setPhotoLink(res.data.data);
+          // console.log("image photo link", res.data);
+          Axios.post(`http://localhost:8080/api/developer/`, {
+            name: firstName + " " + LastName,
+            email: email,
+            password: password,
+            username: username,
+            industry: industry,
+            bio: bio,
+            photoLink: res.data.data,
+          }).catch((err) => {
+            console.log("User creation went wrong.", err);
+          });
         })
         .catch((err) => {
           console.log(err);
         });
-    } else if((role === "Investor")) {
-      Axios.post(`http://localhost:8080/api/investor/`, {
-        name: firstName + " " + LastName,
-        email: email,
-        password: password,
-        username: username,
-        industry: industry,
-        bio: bio,
-        oldStartups: [oldStartUp],
-        amount: amount,
-        photoLink: photoLink,
+      navigate(next);
+    } else if (role === "Investor") {
+      let formData = new FormData();
+      formData.append("photo", tempPhoto);
+      console.log("here is the temp", tempPhoto);
+      Axios.post(`http://localhost:8080/upload/`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
       })
         .then((res) => {
-          console.log(res);
-          navigate(next)
+          Axios.post(`http://localhost:8080/api/investor/`, {
+            name: firstName + " " + LastName,
+            email: email,
+            password: password,
+            username: username,
+            industry: industry,
+            bio: bio,
+            photoLink: res.data.data,
+            oldStartups: [oldStartUp],
+            amount: amount,
+          }).catch((err) => {
+            console.log("User creation went wrong.", err);
+          });
         })
         .catch((err) => {
           console.log(err);
         });
+      navigate(next);
     } else {
-      alert("Please select your role first.")
+      alert("Please select your role before signing up.");
     }
   };
-  // const imageReader = (e) => {
-  //   const reader = new FileReader();
-  //   reader.onload = () => {
-  //     if (reader.readyState == 2) {
-  //       setTempPhoto(reader.result);
-  //     }
-  //   };
-  //   setPhotoLink(reader.readAsDataURL(e.target.files[0]));
-  // };
+
   return (
     <div className="sign-up-container">
       <img
@@ -117,11 +140,28 @@ function Signup() {
           />
         )}
       </div>
+
       {role === "" ? (
         ""
       ) : (
         <>
-            <div className="sign-up-container">
+          <Avatar
+            className="preview-pic"
+            alt="Profile Picture"
+            src={tempPhoto ? URL.createObjectURL(tempPhoto) : ""}
+            sx={{ width: 150, height: 150 }}
+          />
+          <div
+            className="sign-up-container"
+            onSubmit={handleSubmit}
+            encType="multipart/form-data"
+          >
+            <input
+              type="file"
+              id="file"
+              onChange={(e) => setTempPhoto(e.target.files[0])}
+            ></input>
+
             <input
               className="login-input"
               type="text"
@@ -157,7 +197,6 @@ function Signup() {
               value={password || undefined}
               onChange={(e) => setPassword(e.target.value)}
             />
-
             <input
               className="login-input"
               type="text"
@@ -174,7 +213,7 @@ function Signup() {
                 onChange={(e) => setOldStartUp(e.target.value)}
               />
             )}
-                        {role === "Investor" && (
+            {role === "Investor" && (
               <input
                 className="login-input"
                 type="text"
@@ -183,11 +222,14 @@ function Signup() {
                 onChange={(e) => setAmount(e.target.value)}
               />
             )}
-            <button className="submit-button" onClick={handleSubmit}>
+            <button
+              className="submit-button"
+              type="submit"
+              onClick={handleSubmit}
+            >
               Sign Up
             </button>
-            </div>
-
+          </div>
         </>
       )}
       <h5 className="goto-signup">
--- src/components/userProfileBlueArea.js
+++ src/components/userProfileBlueArea.js
@@ -1,32 +1,35 @@
 import "./userProfile.css";
 import { Link } from "react-router-dom";
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import {useNavigate} from 'react-router-dom';
-
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import { useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
 function UserProfileBlueArea(props) {
   const navigate = useNavigate();
-
-  const goBack = ()=> {
-    let goTo = props.mode === "investor" ? '/projects' : '/investors'
+  console.log(props.photoLink);
+  const goBack = () => {
+    let goTo = props.mode === "investor" ? "/projects" : "/investors";
     navigate(goTo); //TODO: change this to the correct param
-  }
+  };
   return (
     <div className="blue-area">
       <ArrowBackIosNewIcon className="back-icon" onClick={goBack} />
       <div className="flex-area">
-      <Link to="/projects">
-        <img
-          className="bmcf-logo"
-          src={require("./img/BuyMeCoffee.png")}
-          alt="BuyMeCoffeeLogo"
+        <Link to="/projects">
+          <img
+            className="bmcf-logo"
+            src={require("./img/BuyMeCoffee.png")}
+            alt="BuyMeCoffeeLogo"
+          />
+        </Link>
+
+        <Avatar
+          className="preview-pic"
+          alt="Profile Picture"
+          src={props.photoLink ? props.photoLink : ""}
+          sx={{ width: 150, height: 150 }}
         />
-      </Link>
-      <img
-        className="profile-pic"
-        src={props.photoLink}
-        alt="UserProfilePicture"
-      />
-      <h3>{props.name}</h3>
+
+        <h3>{props.name}</h3>
       </div>
     </div>
   );
