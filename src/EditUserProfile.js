816d5ecd46396194359283a000a31443220f8163
chayanid Kaewla <ckaewla2@illinois.edu>
2022-12-08 16:15:11 -0600

    edit photo working

--- src/EditUserProfile.js
+++ src/EditUserProfile.js
@@ -4,40 +4,54 @@ import UserProfileWhiteAreaEdit from "./components/userProfileWhiteAreaEdit";
 import { useLocation } from "react-router-dom";
 import Axios from "axios";
 import React from "react";
+import { useState } from "react";
 
 function EditUserProfile(props) {
   let params = useParams();
   const [investor, setInvestor] = React.useState("");
-  const [mode, setMode]=React.useState("")
+  const [mode, setMode] = React.useState("");
+  const [tempPhoto, setTempPhoto] = useState("");
+  const [name, setName] = useState(props.name);
   const location = useLocation();
   React.useEffect(() => {
     if (location.pathname.includes("/investor/profile")) {
-      setMode("investor")
+      setMode("investor");
       Axios.get(
         `http://localhost:8080/api/investor/single_investor/${params.username}`
-      ).then((res) => {
-        console.log(res.data.data[0]);
-        setInvestor(res.data.data[0]);
-      }).catch((err)=>{
-        console.log(err)
-      });
+      )
+        .then((res) => {
+          console.log(res.data.data[0]);
+          setInvestor(res.data.data[0]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     } else {
-      setMode("projectOwner")
-      Axios.get(`http://localhost:8080/api/developer/${params.username}`).then(
-        (res) => {
+      setMode("projectOwner");
+      Axios.get(`http://localhost:8080/api/developer/${params.username}`)
+        .then((res) => {
           console.log(res.data.data);
           setInvestor(res.data.data);
-        }
-      ).catch((err)=>{
-        console.log(err)
-      });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }, [params.username, location.pathname]);
-
+  console.log("name", investor.name);
   return (
     <div className="container-wrap">
-      <UserProfileBlueAreaEdit name={investor.name} photoLink={investor.photoLink} mode={mode} />
+      <UserProfileBlueAreaEdit
+        name={investor.name}
+        photoLink={investor.photoLink}
+        mode={mode}
+        setName={setName}
+        tempPhoto={tempPhoto}
+        setTempPhoto={setTempPhoto}
+      />
       <UserProfileWhiteAreaEdit
+        tempPhoto={tempPhoto}
+        name={name}
         username={params.username}
         person={investor}
         mode={mode}
