cd510efd48002d4bb25895aba5baea8ca2c7d786
aisheem2 <aishee2001@gmail.com>
2022-12-08 16:34:41 -0600

    edit project structure done, waiting on fixed put request

--- src/EditProject.js
+++ src/EditProject.js
@@ -3,16 +3,21 @@ import EditProjectHeader from './components/EditProjectHeader';
 import EditProjectContent from './components/EditProjectContent';
 import { useParams } from 'react-router';
 import axios from 'axios';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function EditProject(props) {
     let params = useParams();
     const [project, setProject] = useState({})
 
-    axios.get("http://localhost:8080/api/project/"+params.projectId).then( (res) => {
+    useEffect( () => {
+      axios.get("http://localhost:8080/api/project/"+params.projectId).then( (res) => {
       //console.log(res.data.data[0]);
       setProject(res.data.data[0]);
-    });
+      }).catch( (err) => {
+        console.log(err);
+      });
+    }, [setProject, params.projectId])
+    
  
     return (
         <div className="project-wrap">
