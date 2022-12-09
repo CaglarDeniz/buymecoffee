0ce8096d5b486a2adff550f1c4b744b3eff43d47
Sanjana <pingali4@illinois.edu>
2022-12-09 15:40:57 -0600

    add other host as well

--- backend/server.js
+++ backend/server.js
@@ -40,17 +40,17 @@ var port = 8080;
 
 // Allow CORS so that backend and frontend could be put on different servers
 var allowCrossDomain = function (req, res, next) {
-	// const corsWhitelist = [
-    //     'http://localhost:3000',
-    //     'https://caglardeniz.gitlab.io/buymecoffee'
-    // ];
-	// if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
-    // res.header("Access-Control-Allow-Origin", req.headers.origin);
-	res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+	const corsWhitelist = [
+        'http://localhost:3000',
+        'https://caglardeniz.gitlab.io'
+    ];
+	if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+	// res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
     res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
 	res.header("Access-Control-Allow-Credentials", "true");
-	// }
+	}
     next();
 };
 app.use(allowCrossDomain);
