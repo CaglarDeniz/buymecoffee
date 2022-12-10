8c2190601b03a787c676434654f807e9819bc9be
chayanid Kaewla <ckaewla2@illinois.edu>
2022-12-10 14:48:53 -0600

    responsive userprofile

--- src/components/addProjects.js
+++ src/components/addProjects.js
@@ -5,28 +5,35 @@ import { Link } from "react-router-dom";
 import "./addProjects.css";
 
 function AddProjects(props) {
-    return (
-        <Grid className="add-extra-margin" item xs={12} sm={6} md={6} key={props.id}>
-        <Link className="link user-link" to={`/submitProject`}>
-          <Card 
-            sx={{
-              minWidth: props.projectList?.length === 0 ? 165.825 : "",
-              width:"100%",
-              maxWidth: 345,
-              minHeight: 198,
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              ":hover": {
-                boxShadow: 10,
-              },
-            }}
-          >
-            <AddIcon className="add-icon"/>
-          </Card>
-        </Link>
-      </Grid>
-    )
+  return (
+    <Grid
+      className="add-extra-margin my-project-center"
+      item
+      xs={12}
+      sm={6}
+      md={3}
+      key={props.id}
+    >
+      <Link className="link user-link add-project" to={`/submitProject`}>
+        <Card
+          sx={{
+            minWidth: props.projectList?.length === 0 ? 165.825 : "",
+            width: "100%",
+            maxWidth: 345,
+            minHeight: 201.125,
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            ":hover": {
+              boxShadow: 10,
+            },
+          }}
+        >
+          <AddIcon className="add-icon" />
+        </Card>
+      </Link>
+    </Grid>
+  );
 }
 export default AddProjects;
